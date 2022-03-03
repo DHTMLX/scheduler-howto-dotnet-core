e072446816be89327a6ea29082c77aa5f721956e
Maksim Lakatkou <90101782+MaksimLakatkou@users.noreply.github.com>
2022-03-03 15:18:43 +0300

    Add files via upload

---
+++ SchedulerApp/Models/SchedulerInitializerExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace SchedulerApp.Models
+{
+    public static class SchedulerInitializerExtension
+    {
+        public static IHost InitializeDatabase(this IHost webHost)
+        {
+            var serviceScopeFactory =
+            (IServiceScopeFactory?)webHost.Services.GetService(typeof(IServiceScopeFactory));
+
+            using (var scope = serviceScopeFactory!.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var dbContext = services.GetRequiredService<SchedulerContext>();
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+                SchedulerSeeder.Seed(dbContext);
+            }
+
+            return webHost;
+        }
+    }
+}
--- SchedulerApp/Models/WebApiEvent.cs
+++ SchedulerApp/Models/WebApiEvent.cs
@@ -2,35 +2,27 @@
 
 namespace SchedulerApp.Models
 {
-    public class WebAPIRecurring
+    public class WebAPIEvent
     {
         public int id { get; set; }
         public string? text { get; set; }
         public string? start_date { get; set; }
         public string? end_date { get; set; }
 
-        public int? event_pid { get; set; }
-        public string? rec_type { get; set; }
-        public long? event_length { get; set; }
-
-        public static explicit operator WebAPIRecurring(SchedulerRecurringEvent ev)
+        public static explicit operator WebAPIEvent(SchedulerEvent ev)
         {
-            return new WebAPIRecurring
+            return new WebAPIEvent
             {
                 id = ev.Id,
                 text = HtmlEncoder.Default.Encode(ev.Name != null ? ev.Name : ""),
                 start_date = ev.StartDate.ToString("yyyy-MM-dd HH:mm"),
                 end_date = ev.EndDate.ToString("yyyy-MM-dd HH:mm"),
-
-                event_pid = ev.EventPID,
-                rec_type = ev.RecType,
-                event_length = ev.EventLength
             };
         }
 
-        public static explicit operator SchedulerRecurringEvent(WebAPIRecurring ev)
+        public static explicit operator SchedulerEvent(WebAPIEvent ev)
         {
-            return new SchedulerRecurringEvent
+            return new SchedulerEvent
             {
                 Id = ev.id,
                 Name = ev.text,
@@ -38,10 +30,6 @@ namespace SchedulerApp.Models
                     System.Globalization.CultureInfo.InvariantCulture) : new DateTime(),
                 EndDate = ev.end_date != null ? DateTime.Parse(ev.end_date,
                     System.Globalization.CultureInfo.InvariantCulture) : new DateTime(),
-
-                EventPID = ev.event_pid != null ? ev.event_pid.Value : 0,
-                EventLength = ev.event_length != null ? ev.event_length.Value : 0,
-                RecType = ev.rec_type
             };
         }
     }
